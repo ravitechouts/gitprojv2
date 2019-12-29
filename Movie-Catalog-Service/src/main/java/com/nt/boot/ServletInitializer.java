6bb770b73fb5cea6aab46d0bc0156aef8e0bf074
unknown <raviranjan.listserv@gmail.com>
2019-12-29 18:27:17 +0530

    uploading project movie catalog service

---
+++ Movie-Catalog-Service/src/main/java/com/nt/boot/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nt.boot;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MovieCatalogServiceApplication.class);
+	}
+
+}
